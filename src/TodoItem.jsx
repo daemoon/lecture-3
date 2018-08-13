75f2d0ccebe2e0418ff13f9b7bfa255312911eb4
Martin Knapik <martink4@kentico.com>
2018-08-13 12:03:41 +0200

    Added finishing tasks, redid func to component, added state, render, ctor

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,5 +1,21 @@
 import * as React from 'react';
 
-export function TodoItem(props) {
-    return (<li>{props.value}</li>)
+export class TodoItem extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            done: false,
+        };
+    }
+
+    doItem = () => {
+        this.setState({
+            done: true,
+        })
+    };
+
+    render() {
+        return (<li onClick={this.doItem}>{this.props.value}<input type="checkbox" checked={this.state.done}/></li>)
+    }
 }
