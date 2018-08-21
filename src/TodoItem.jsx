44706acc80ab2c3520ae757993ab1deb66e62b06
Martin Knapik <martink4@kentico.com>
2018-08-21 13:08:36 +0200

    Added audio on completion of item

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -1,24 +1,31 @@
 import * as React from 'react';
 
 export class TodoItem extends React.PureComponent {
+    _audio;
     constructor(props) {
         super(props);
 
         this.state = {
             done: false,
         };
+        this._audio = React.createRef();
     }
 
     doItem = () => {
         this.setState({
             done: true,
-        })
-        this.props.save(true);
+        });
+        this._audio.current.play();
     };
 
     render() {
-        return (<li onClick={this.doItem}>{this.props.value}<input type="checkbox"
-                                                                   checked={this.state.done}/>{this.props.savingStatus !== 'none' && this.props.savingStatus}
+        return (
+
+        <li onClick={this.doItem}>
+            <audio src="https://www.myinstants.com/media/sounds/-click-nice_3.mp3" ref={this._audio} />
+            {this.props.value}
+            <input type="checkbox" checked={this.state.done}/>
+            {this.props.savingStatus !== 'none' && this.props.savingStatus}
         </li>)
     }
 }
